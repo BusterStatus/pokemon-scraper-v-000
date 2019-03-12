4a5291fd2c18e4a66e4ebb6d14f0f90e5f7f6fa2
matthew.serre <matthew.serre@gmail.com>
2019-03-12 17:16:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,7 @@ class Pokemon
     pokemon = db.execute("select * from pokemon where id = ?", id).flatten
     name = pokemon[1]
     type = pokemon[2]
-    Pokemon.new(id: id, name: name, type: type, db: db, hp: hp)
+    Pokemon.new(id: id, name: name, type: type, db: db, hp: nil)
   end
   
   def alter_hp(new_hp, db)
